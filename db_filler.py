0c1adfd114b5eea68efac413e871b4f15b300181
Jlo6CTEP <35602222+Jlo6CTEP@users.noreply.github.com>
2018-11-11 14:35:31 +0300

    sample data generation

--- db_filler.py
+++ db_filler.py
@@ -4,21 +4,67 @@ import re
 import subprocess as sb
 import glob
 import os
+import random
+
+return_types = {'integer': lambda x: ''.join(list(map(int, x))),
+                'string': lambda x: ''.join(list(map(str, x))),
+                'chr': lambda x: ''.join(list(map(chr, x)))}
+
+nonterm_txt = [files for paths, dirs, files in os.walk('.\\sample_data\\nonterminal')][0]
+nonterm_notxt = [x.replace('.txt', '') for x in nonterm_txt]
+nonterm_path = [os.path.join('.\\sample_data\\nonterminal', file) for file in nonterm_txt]
+
+nonterm_files = {name: file for name, file in zip(nonterm_notxt, [open(x, 'r').read() for x in nonterm_path])}
+
+term_txt = [files for paths, dirs, files in os.walk('.\\sample_data\\terminal')][0]
+term_notxt = [x.replace('.txt', '') for x in term_txt]
+term_path = [os.path.join('.\\sample_data\\terminal', file) for file in term_txt]
+term_files = {name: file for name, file in zip(term_notxt, [open(x, 'r').read().split(',') for x in term_path])}
+
+
+def handle_token(term):
+    if term in term_notxt:
+        return term_files[term][random.randint(0, len(term_files[term]) - 1)]
+    elif re.sub('\(.*\)', '', term) in nonterm_notxt:
+        term_no_args = re.sub('\(.*\)', '', term)
+        args = re.sub(term_no_args + '\(|\)', '', term).replace(' ', '').split(',')
+        return_type = return_types[nonterm_files[term_no_args].split(' ')[0].strip(' ')]
+        if re.fullmatch('int\(\d*,\d*\)', term):
+            return random.randint(*map(int, args))
+        else:
+            parsed = []
+            literal = ""
+            to_parse = ' '.join(nonterm_files[term_no_args].split(' ')[1:])
+
+            regex = re.compile('|'.join(sorted(term_notxt + nonterm_notxt, key=lambda x: 1 / len(x))))
+            while len(to_parse) > 0:
+                repl = re.match(regex, to_parse)
+                if repl:
+                    repl = repl.group()
+                    if len(literal) > 0:
+                        parsed.append(literal)
+                        literal = ''
+                    if repl in term_notxt:
+                        to_parse = to_parse.replace(repl, '', 1)
+                        parsed.append(repl)
+                    elif repl in nonterm_notxt:
+                        parsed.append(to_parse[:to_parse.index(')') + 1])
+                        to_parse = to_parse[to_parse.index(')') + 1:]
+                else:
+                    literal += to_parse[0]
+                    to_parse = to_parse[1:]
+            return return_type([handle_token(x) for x in parsed if x])
+    else:
+        return term
 
 
 def main(*args):
     print('Prompt templates to fill db')
 
-    generic_txt = [files for paths, dirs, files in os.walk('.\\sample_data\\generic')][0]
-    generic_notxt = [x.strip('.txt') for x in generic_txt]
+    f = nonterm_files
+    k = term_files
 
-    static_txt = [files for paths, dirs, files in os.walk('.\\sample_data\\static')][0]
-    static_notxt = [x.strip('.txt') for x in static_txt]
-
-    print(generic_notxt)
-    print(generic_txt)
-    print(static_notxt)
-    print(static_txt)
+    print(''.join(handle_token('license_plate()')))
 
     # print(args)
     # db_name = args[0][1].replace("\\\\", '\\')
--- text.py
+++ text.py
@@ -1,8 +1,6 @@
+import random
 import re
-file = r".\sample_data\static\shape.txt"
-k = open(file, 'r').read()
 
-k = k.replace('\n', ',')
-
-#print(f)
+file = r".\sample_data\terminal\email.txt"
+k = open(file, 'r').read().replace('\n', ',')
 open(file, 'w').write(k)
