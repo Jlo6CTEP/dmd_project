3991289bd1f4f3da653cd471218e313e3f490a80
Jlo6CTEP <19yura99@mail.ru>
2018-11-12 17:54:24 +0300

    db filling and creation ready

--- courseDB/database_filler/test.py
+++ courseDB/database_filler/test.py
@@ -1,5 +1,4 @@
-import random
-x = lambda: random.sample(range(1, 100), 1)
-f = []
-[f.append(x()[0]) for l in range(50)]
-print([f.count(x) for x in range(100)])
+f = open('.\\sample_data\\terminal\\color.txt', 'r').read()
+f = f.replace('color_', '')
+print(f)
+f = open('.\\sample_data\\terminal\\color.txt', 'w').write(f)
