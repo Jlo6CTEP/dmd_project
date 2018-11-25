e3ff8089d8eaf673b9187f37cef82258054079df
Alexander Gruk <gruk98@mail.ru>
2018-11-26 01:33:35 +0300

    Querries begin

--- query/query.py
+++ query/query.py
@@ -2,21 +2,38 @@ import task_list
 import sqlite3
 import os
 
-DB_PATH = os.getcwd()[:-5] + "courseDB/courseDB"
+DB_PATH = "../courseDB/courseDB"
+DP_PATH = os.getcwd()[:-5] + "courseDB/courseDB"
 
 
 # Each queryN method returns string output without separators and stuff
 
+
 def query1():
-    cursor.execute("SELECT * FROM Customer")
+    cursor.execute("SELECT * FROM car WHERE color = 'red'")
     res = cursor.fetchall()  # getting results
     res = "\n".join([str(x) for x in res])
-
     return res
 
 
-def query2():
+def query2(given_date):  # YYYY-MM-DD
+    cursor.execute(
+        "SELECT stationId, start_time, end_time FROM charge WHERE date(start_time) = {} or date(end_time) = {}".format(
+            given_date, given_date))
+    used = [set() for h in range(24)]
+    for rec in cursor.fetchall():
+        print(rec)
+
     return "test2"
+
+
+for i in range(x +):
+
+
+# code
+
+def query3():
+    return "test3"
 
 
 def do_query(query_name, input_data=None):
@@ -37,3 +54,8 @@ def do_query(query_name, input_data=None):
 
     conn.close()  # closing connection with DB
     return res
+
+
+do_query("Query 1 ")
+do_query("Query 2 ", "2018-11-01")
+# do_query("Query 3 ")
